24996ad1b9a95e45c3a611a59a34fbd46150217b
tomas-sedlak <t.sedlak.24@gmail.com>
2023-11-24 11:39:22 +0100

    Added login and signup routes

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require("express")
 app = express()
 
+app.set("view engine", "ejs")
+
+// Express middlewares
+app.use(express.static("./public"))
+
 app.get("/", (req, res) => {
     res.send("Srodo homepage")
 })
 
-const port = 3000
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}...`)
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+
+app.get("/signup", (req, res) => {
+    res.render("signup")
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}...`)
 })
