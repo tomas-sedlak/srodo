c431f7a10593c86d41a0205ad8b38de8744c73e9
tomas-sedlak <t.sedlak.24@gmail.com>
2023-11-27 19:35:33 +0100

    Adde user model

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    displayName: String,
+    email: String,
+    loginMethod: String,
+    local: {
+        hashedPassword: String,
+        salt: String,
+        emailVerified: Boolean,
+        emailVerificationCode: String,
+        passwordResetToken: String,
+        passwordResetTimestamp: Date
+    },
+    google: {
+        googleId: String,
+        accessToken: String
+    },
+    facebook: {
+        facebookId: String,
+        accessToken: String
+    },
+    timestamp: {
+        type: Date,
+        default: () => Date.now()
+    },
+    profilePicture: {
+        type: String,
+        default: () => "default.jpg"
+    },
+})
+
+module.exports = mongoose.model("User", userSchema)
